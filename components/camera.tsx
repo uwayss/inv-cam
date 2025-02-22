66a9529e7c78c96fc1ddb0b6e7fddb612b3aa129
Muhammed <antar.muhammed1@gmail.com>
2025-02-22 17:20:57 +0300

    feat: Add color inversion filter to camera frame processing

--- components/camera.tsx
+++ components/camera.tsx
@@ -6,11 +6,29 @@ import {
   useSkiaFrameProcessor,
 } from "react-native-vision-camera";
 
+const horizontalFlipFilter = Skia.RuntimeEffect.Make(`
+  uniform shader image;
+  half4 main(vec2 pos) {
+    vec4 color = image.eval(pos);
+    return vec4((1.0 - color).rgb, 1.0);
+  }
+`);
+const shaderBuilder = horizontalFlipFilter
+  ? Skia.RuntimeShaderBuilder(horizontalFlipFilter)
+  : null;
+const imageFilter = shaderBuilder
+  ? Skia.ImageFilter.MakeRuntimeShader(shaderBuilder, null, null)
+  : null;
+const paint = Skia.Paint();
+paint.setImageFilter(imageFilter);
 export default function MyCamera({ device }: { device: CameraDevice }) {
-  const frameProcessor = useSkiaFrameProcessor((frame) => {
-    "worklet";
-    frame.render();
-  }, []);
+  const frameProcessor = useSkiaFrameProcessor(
+    (frame) => {
+      "worklet";
+      frame.render(paint);
+    },
+    [paint],
+  );
   return (
     <Camera
       style={StyleSheet.absoluteFill}
